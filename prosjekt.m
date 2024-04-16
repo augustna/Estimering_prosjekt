e1761347e6c954c52e1af0069c42c7cee16d20b2
jorgehav <156174461+jorgehav@users.noreply.github.com>
2024-04-16 14:04:56 +0200

    Add files via upload

--- prosjekt.m
+++ prosjekt.m
@@ -4,27 +4,43 @@ x3all = load('class_3','-ascii');
 xAllTable = readtable('iris.data', 'FileType', 'text', 'Format', '%f%f%f%f%C', 'ReadVariableNames', false);
 
 
-disp(T);
 
 % Target vectors
-t1 = [1 0 0]';
-t2 = [0 1 0]';
-t3 = [0 0 1]';
+t_k = [1 0 0; 
+        0 1 0; 
+        0 0 1];
 
-%{
-%MSE
-MSE = 0.5 * 
-
+N = 30; %Training 30 samples
+C = 3; %number of classes
 alpha = 0.1;
 
-%m = ? Tror man kan velge m fritt. Iterations
-m = 150;
+%Init
+grad_W_MSE = 0;
+W = 0;
+z_k = W*x_k;
 
-%W0 = ?
-W0 = [];
-%}
-% Definere sigmoid funksjon
-m = 5;
-for k = 1:m
+%Sigmoid
+g_k = 1/(1+exp(-z_k));
+
+%Gradients
+grad_gk_MSE = g_k-t_k;
+grad_zk_g = g_k * (1-g_k);
+grad_W_zk = x_k';
+
+
+m = 1;
+while m < 100
     
+
+    for k = 1:N
+        grad_W_MSE = grad_gk_MSE*grad_zk_g*grad_W_zk;
+    end
+    grad_gk_MSE = g_k-t_k;
+    grad_zk_g = gk * (1-g_k);
+    grad_W_zk = x_k';
+    W = W -alpha*grad_W_MSE;
+
+    m = m +1;
 end
+
+

d36a52d59a4ac1e5646619e0f9cdc160eedfb9a0
August Natvik <augustna@stud.ntnu.no>
2024-04-25 17:38:56 +0200

    Done with 2a I think. Clustered code should be going faster I think. Optimize the code

--- Num_cl/main.m
+++ Num_cl/main.m
@@ -1,5 +1,6 @@
-key = cell(10,1);
+tic;
 
+key = cell(10,1);
 k = 1;
 test_chunk = testv(1000*(k-1) + 1:k*1000, :);
 
@@ -18,11 +19,6 @@ misclassifications = find(key{1} ~= testlab(1:1000, :));
 misclassified_img = zeros(28,28); misclassified_img(:)= testv(1,:);
 misclassified_img2 = zeros(28,28); misclassified_img(:)= testv(1,:);
 
-%img = image(misclassified_img);
-
-% Clustering
-M = 64;
-group = groupMatrixRowsByVector(trainv, trainlab);
-% C_i contains clustered data
-[idx_i, C_i] = kmeans(group{1}, M);
+toc;
+% Elapsed time about 7.4 seconds
 
--- Num_cl/main2.m
+++ Num_cl/main2.m
@@ -1,10 +1,41 @@
+k = 1;
+test_chunk = testv(1000*(k-1) + 1:k*1000, :);
+
 % Clustering
 M = 64;
+tic;
 group = groupMatrixRowsByVector(trainv, trainlab);
 % C_i contains clustered data
-[idx_i, C_i] = kmeans(group{1}, M);
+
 
 % Finding
 
-for i 
-distances_clustered = pdist2(test_chunk, C_i);
+% Iterating through each class
+% For each class checking if the current class cluster is nearer the sample
+% than the nearest this far
+% If the sample is nearer the current class than the one in nearest class,
+% current_min is updated and nearest_class is updated
+for class = 1:10
+    [idx_i, C_i] = kmeans(group{class}, M);
+    distances_clustered = pdist2(test_chunk, C_i);
+    [class_min, colIndices] = min(distances_clustered, [], 2);
+    
+    if class == 1 
+       nearest_class = ones(1000, 1);
+       current_min = class_min;
+    end
+    
+    for row = 1:1000
+        if class_min(row) < current_min(row)
+            current_min(row) = class_min(row);
+            nearest_class(row) = class;
+        end
+    end
+end
+
+disp(nearest_class);
+
+conf_matrix_clustered = confusionMatrix(nearest_class, testlab(1:1000));
+error_rate = calculateErrorRate(conf_matrix_clustered);
+toc;
+% Elapsed time about 12.1158943 seconds
